e0f02e3d48b1e41b961986156dfbd224d06e2b5b
Muhammad Muzammil <126906348+muhammad-muzammil-123@users.noreply.github.com>
2023-09-28 09:07:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,206 @@
+// #*#*#*#*#*#*#*#*#*#* CHAPTER (17) TO (20) #*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+// ######################## QUESTION NO: 1 ##############
+//   var arr = [[], [[]],[]];
+//   console.log(arr[1])
+
+// #################QUESSTION NO:2 ####################
+// var arr = [[0,1,2,3],[1,0,1,2],[1,2,1,3]]
+// console.log(arr[1][2])
+
+// ###################### QUESTION NO :3 #################
+// NUMERIC VALUE FROM ARRAY
+// var numericValue = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numericValue)
+// NUMERIC VALUE FROM LOOPS
+  
+// for(var i = 1; i <=  10; i ++){
+//     console.log(i)
+// }
+ 
+// ################## QUESTION NO: 4 ####################
+//  var tableName = prompt("enter a number for table");
+//  var tableLength = prompt("enter a length for table");
+//  for(var i = 1; i <= tableLength; i ++){
+//   document.write(tableName + " X" +  i + "  = " + i*tableName + "<br>");
+//  }
+
+
+// #################### QUESTION NO: 5 #######################
+// var userInput = prompt("Enter a fruit name")
+//  var fruitsName = ["apple","mango","Orange","Banana"];
+//  var flags = false;
+//  for(var i = 0; i < fruitsName.length; i++){
+//     if(userInput === fruitsName[0]){
+      
+//            flags = true
+  
+//  }
+// } 
+// if(flags){
+//     console.log("this index is ",fruitsName[i]);
+// }
+// if(flags){
+//     console.log("this index is ", fruitsName[i]);
+// }if(userInput === fruitsName[2]){
+//     console.log("this index is two");
+// }if(userInput === fruitsName[3]){
+//     console.log("this index is three");
+    
+// }
+  
+// ###################### QUESTION NO : 6 ####################
+    //          (a)
+// for(var i = 1; i <= 15; i++){
+//     console.log(i);
+// }
+            //  (b)
+// for(var i = 10; i >= 1; i--){
+//     console.log(i)
+// }               
+            //  (c)
+// for(var i = 0; i <= 10; i++){
+//     console.log(i*2)
+// }
+             // (d)
+// for(var i = 1; i <= 10; i++){
+//     console.log((i*2)-1)
+// }
+            //  (e)
+// for(var i = 1; i <= 10; i++ ){
+//     console.log(i*2000)
+// }
+
+// ####################### QUESTION NO:7 #################
+// var userInput = prompt("welcome to this backery.. What do you want sir/madam");
+// var arr = ["cake","applepie","cookie","chips","patties"];
+// var flags = false
+// for(var i = 0; i < arr.length; i++){
+//     if(userInput ===arr[i]){
+//        flags = true
+//     }
+// }if(flags){
+//      console.log(userInput + " is available at index " + arr.indexOf(userInput))
+// }else{
+//         console.log(userInput + " is not available")
+//     }
+
+// ########################## QUESTION NO: 8 ####################
+// for(var i = 1; i <= 20; i ++){
+// console.log(i*5)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
